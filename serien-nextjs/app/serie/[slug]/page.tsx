d8c85d9eae264246ba9db2c4d3659a62311cd1ae
serien-de <dev@serien.de>
2026-02-25 11:06:22 +0000

    auto-commit for 27541c98-0631-4a72-9e0c-22728d1b54c6

--- serien-nextjs/app/serie/[slug]/page.tsx
+++ serien-nextjs/app/serie/[slug]/page.tsx
@@ -122,6 +122,11 @@ export default async function SeriesDetailPage({ params }: PageProps) {
                   </div>
                 )}
               </div>
+              
+              {/* Follow Button */}
+              <div className="mt-6">
+                <FollowButton tmdbId={series.tmdbId} seriesName={series.name || ''} />
+              </div>
             </div>
           </div>
         </div>
