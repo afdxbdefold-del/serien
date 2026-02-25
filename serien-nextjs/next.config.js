5fb7da61d8749c35dfd1907bcf57b0bb4cd19491
serien-de <dev@serien.de>
2026-02-25 08:50:37 +0000

    auto-commit for 79269be9-0b86-4b1d-80e2-122cc06bee11

---
+++ serien-nextjs/next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
