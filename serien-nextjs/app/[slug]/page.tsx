9df002c3cbfb1005b34b5d058689deed554192d8
serien-de <dev@serien.de>
2026-02-25 11:06:35 +0000

    auto-commit for 9c5bb3ed-bef5-49df-a124-07321d5ec9fb

--- serien-nextjs/app/[slug]/page.tsx
+++ serien-nextjs/app/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import { ArrowLeft, Clock, Share2 } from 'lucide-react';
 import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
+import CommentsSection from '@/components/CommentsSection';
 
 interface PageProps {
   params: Promise<{
