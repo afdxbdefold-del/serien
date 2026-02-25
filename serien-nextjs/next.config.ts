bcd148706dc58a6a56f5d899e4b0f7e6967c0536
serien-de <dev@serien.de>
2026-02-25 05:02:48 +0000

    auto-commit for e9548b89-9d0b-4231-8fe4-4209422df908

--- serien-nextjs/next.config.ts
+++ serien-nextjs/next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from 'next';
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '**.vercel-storage.com' }
+      { 
+        protocol: 'https', 
+        hostname: 'images.unsplash.com' 
+      },
+      { 
+        protocol: 'https', 
+        hostname: '**.vercel-storage.com' 
+      }
     ]
   }
 };
+
 export default nextConfig;
