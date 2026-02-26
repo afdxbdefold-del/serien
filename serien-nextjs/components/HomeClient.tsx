f70c84ec03592c4ec88c9f5c77a0222108d7df7d
serien-de <dev@serien.de>
2026-02-26 07:28:10 +0000

    auto-commit for 0876e82f-b8dc-4d30-bef9-73f68993db73

--- serien-nextjs/components/HomeClient.tsx
+++ serien-nextjs/components/HomeClient.tsx
@@ -172,11 +172,8 @@ export default function HomeClient({ initialNews, initialSeries, stats, isAuthen
                 href="/serienfinder"
                 className="inline-flex items-center gap-2 px-8 py-3 bg-gradient-to-r from-cyan-500 to-blue-500 text-white rounded-lg font-semibold hover:from-cyan-600 hover:to-blue-600 transition-all shadow-lg hover:shadow-xl"
               >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
-                  <path fillRule="evenodd" d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z" clipRule="evenodd" />
-                </svg>
-                Serien entdecken
+                <Sparkles className="h-5 w-5" />
+                Serienfinder
               </Link>
             </div>
           </div>
