65c48bc8e81c72c52191cabffb7885d7f0469813
serien-de <dev@serien.de>
2026-02-26 12:18:31 +0000

    auto-commit for e13d22ee-8f30-4df6-a579-47df9a410869

--- serien-nextjs/app/[slug]/page.tsx
+++ serien-nextjs/app/[slug]/page.tsx
@@ -314,7 +314,7 @@ export default async function ArticlePage({ params }: PageProps) {
                 </div>
               </Link>
             </div>
-          </div>
+          </section>
         )}
 
         {/* Where to Stream Box */}
