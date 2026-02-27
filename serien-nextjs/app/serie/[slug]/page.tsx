7b40f71c283a1e420cdd4dbb9689ac5ad49313dd
serien-de <dev@serien.de>
2026-02-27 03:27:05 +0000

    auto-commit for 20a9e01c-21b5-43c8-bd56-4c92d20d20c9

--- serien-nextjs/app/serie/[slug]/page.tsx
+++ serien-nextjs/app/serie/[slug]/page.tsx
@@ -374,6 +374,13 @@ export default async function SeriesDetailPage({ params }: PageProps) {
             )}
           </div>
 
+          {/* Series Q&A Section (Desktop) */}
+          {seriesQA && seriesQA.length > 0 && (
+            <div className="lg:col-span-7 mt-8">
+              <SeriesQA questions={seriesQA} seriesName={series.name || series.title} />
+            </div>
+          )}
+
           {/* RIGHT: Hero Box (Sticky on Desktop) */}
           <div className="hidden lg:block lg:col-span-5">
             <div className="lg:sticky lg:top-6">
