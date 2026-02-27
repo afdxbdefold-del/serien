c6db9085df7c60e7e9a3888263e2e8ca21aefc3b
serien-de <dev@serien.de>
2026-02-27 02:31:05 +0000

    auto-commit for 8a7d47fa-9c31-4bba-9693-a7317a9223a9

--- serien-nextjs/components/InlineVideoPlayer.tsx
+++ serien-nextjs/components/InlineVideoPlayer.tsx
@@ -31,7 +31,7 @@ export default function InlineVideoPlayer({ heroImageUrl, trailerUrl, title }: I
 
   // Build absolute video URL
   const baseUrl = typeof window !== 'undefined' ? window.location.origin : '';
-  const videoUrl = `${baseUrl}/trailer/${trailerUrl}`;
+  const videoUrl = `${baseUrl}/api/trailer/${trailerUrl}`;
 
   // With trailer: Show image with play button, then video
   return (
