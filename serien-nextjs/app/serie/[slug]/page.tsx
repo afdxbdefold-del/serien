af96fb374696d981837349cc5f7b8b3497e709d3
serien-de <dev@serien.de>
2026-02-28 15:39:42 +0000

    auto-commit for 28c78264-b310-4680-b821-2c88f64a07a0

--- serien-nextjs/app/serie/[slug]/page.tsx
+++ serien-nextjs/app/serie/[slug]/page.tsx
@@ -240,7 +240,7 @@ export default async function SeriesDetailPage({ params }: PageProps) {
         </div>
 
         {/* NEW: Seasons & Status */}
-        <div className="px-6 mb-6">
+        <div className="sm:px-6 mb-6">
           <SeasonsStatus
             seriesName={series.name || series.title}
             seasons={seasons}
