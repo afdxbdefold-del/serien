e455ae112351ca50477f67faa4453454a9b39184
serien-de <dev@serien.de>
2026-03-01 12:23:26 +0000

    auto-commit for 6af316ea-3fee-4963-ab3a-67c1a839f563

---
+++ serien-nextjs/lib/pipeline/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Pipeline Modules Index
+ * Central export point for all pipeline modules
+ */
+
+export * from './article-creator';
+export * from './post-processors';
+export * from './content-workflow';
+export * from './utils';
