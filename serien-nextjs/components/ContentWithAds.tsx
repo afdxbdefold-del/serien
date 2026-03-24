b1a3b5f8fa1e5eae58030b4317dafdfbe3e476da
serien-de <dev@serien.de>
2026-03-24 13:13:37 +0000

    auto-commit for 7373b543-fe12-4cc9-8303-6828dcdc3d9c

--- serien-nextjs/components/ContentWithAds.tsx
+++ serien-nextjs/components/ContentWithAds.tsx
@@ -45,10 +45,9 @@ export default function ContentWithAds({
         adContainer.className = 'content-ad-unit my-6 flex justify-center not-prose';
         adContainer.innerHTML = `
           <ins class="adsbygoogle"
-               style="display:block;width:300px;min-height:250px"
+               style="display:inline-block;width:300px;height:250px"
                data-ad-client="ca-pub-8583619451045805"
-               data-ad-slot="9591890570"
-               data-full-width-responsive="false"></ins>
+               data-ad-slot="9591890570"></ins>
         `;
 
         // Insert after paragraph
